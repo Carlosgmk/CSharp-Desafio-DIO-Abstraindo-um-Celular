1d67df3b74c1cf0650d3b76568385c988d560e0a
Macedo <gm18242@gmail.com>
2024-09-12 14:42:28 -0300

    Exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using DesafioPOO.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("123456", "Nokia Model", "1111111111", 64);
+        Smartphone iphone = new Iphone("654321", "iPhone Model", "2222222222", 128);
+
+        Console.WriteLine("Smartphone Nokia:");
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone:");
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
